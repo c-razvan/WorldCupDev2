99d4e2978e419b8b0fa2e2275427631cb00158f2
c-razvan <razvan.chirciu@icloud.com>
2023-07-19 20:51:03 +0300

    -more sponsors

--- scripts/sponsor-slider.js
+++ scripts/sponsor-slider.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 10000,
+        autoplaySpeed: 7000,
         arrows: false,
         dots: false,
         pauseOnHover: false,
--- scripts/sponsori.js
+++ scripts/sponsori.js
@@ -1,8 +1,8 @@
 let arr = ["OMV.png","RG.png","FCC.png","RENOV.png","VNT.svg","BRT.jpeg","BSNL.png","TAN.jpg","TEP.svg","TOY.png","TVR.png","BUCO.png","FG.png",
             "EM.png","LS.png","LS.png","Maspex.png","NEST.png","LOGO NOU.jpg","LM.png","MON.png","RG.png","SC.png","SPIETH.png","STERG.png",
-            "QS.jpg","AP.png","AR.png","IWA.png","UZ.png"]
+            "QS.jpg","AP.png","AR.png","IWA.png","UZ.png","MED.png"]
 
-for(let i=0; i<arr.length;i++){
+for(let i=0; i<arr.length; i++){
 
     let element2 = document.createElement("div");
     element2.setAttribute("class","slide")
@@ -12,5 +12,18 @@ for(let i=0; i<arr.length;i++){
 
     element2.appendChild(img)
     document.getElementById("append-in").appendChild(element2)
+
+}
+
+for(let i=arr.length - 1; i > 0 ; i--){
+
+    let element2 = document.createElement("div");
+    element2.setAttribute("class","slide")
+    let img = document.createElement("img")
+    img.setAttribute("class","img")
+    img.setAttribute("src",`./assets/Sponsor Logos/${arr[i]}`)
+
+    element2.appendChild(img)
+    document.getElementById("append-in-2").appendChild(element2)
 
 }
