2ffc2ac5b30612656e58d43301b51fb035ac58ae
c-razvan <razvan.chirciu@icloud.com>
2023-07-19 20:38:14 +0300

    -navbar js added

---
+++ scripts/nav-bar-element.js
@@ -0,0 +1,41 @@
+let arrSponsori = ["home", "live","event files","results","gallery","volunteer","contact"]
+
+for(let i=0; i<arrSponsori.length;i++){
+
+    let element = document.createElement("li");
+    element.setAttribute("class", "nav-item");
+
+    let link = document.createElement("a");
+    link.setAttribute("class","nav-link link text-danger text-primary display-6 header-button");
+    link.setAttribute("href",`./${arrSponsori[i]}.html`);
+    link.innerHTML = `${arrSponsori[i].toUpperCase()}`;
+
+    if(arrSponsori[i] === "home"){
+        link.setAttribute("href",`./index.html`);
+    }
+
+    element.appendChild(link)
+
+    document.getElementById("navbar-append").appendChild(element)
+
+}
+
+for(let i=0; i<arrSponsori.length;i++){
+
+    let element = document.createElement("li");
+    element.setAttribute("class", "nav-item");
+
+    let link = document.createElement("a");
+    link.setAttribute("class","nav-link link text-danger text-primary display-6 header-button");
+    link.setAttribute("href",`./${arrSponsori[i]}.html`);
+    link.innerHTML = `${arrSponsori[i].toUpperCase()}`;
+
+    if(arrSponsori[i] === "home"){
+        link.setAttribute("href",`./index.html`);
+    }
+
+    element.appendChild(link)
+
+    document.getElementById("navbar-append2").appendChild(element)
+
+}
