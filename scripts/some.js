600c3eb6eb2a87b436d5b837cfb3ba0f9792825f
c-razvan <razvan.chirciu@icloud.com>
2023-10-12 15:19:58 +0300

    -up

--- scripts/some.js
+++ scripts/some.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("oct 11, 2023 09:00:00").getTime();
+var countDownDate = new Date("oct 13, 2023 10:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
